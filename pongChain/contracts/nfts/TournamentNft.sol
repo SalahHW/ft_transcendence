6395c380e107599b44767fa9d6d66d7cba4b0462
joel688 <joachimkoenig68@icloud.com>
2025-03-14 17:25:42 +0100

    refactor: simplify TournamentNft contract by removing unused variables and updating minting logic; add new test file for TournamentNft functionality

--- pongChain/contracts/nfts/TournamentNft.sol
+++ pongChain/contracts/nfts/TournamentNft.sol
@@ -9,26 +9,20 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  * @dev ERC721 token representing a tournament trophy, non-transferable.
  */
 contract TournamentNft is ERC721, Ownable {
-    uint256 public nextTokenId;
-    mapping(uint256 => uint256) public tournamentIds;
-
     event TournamentNftMinted(
         address indexed winner,
         uint256 tokenId,
         uint256 tournamentId
     );
 
-    constructor() ERC721("Tournament Trophy", "TNT") Ownable(msg.sender) {
-        nextTokenId = 1;
-    }
+    constructor() ERC721("Tournament Trophy", "TNT") Ownable(msg.sender) {}
 
     function mintTournamentNft(
         address winner,
-        uint256 tournamentId
+        uint256 tournamentId,
+        uint256 tokenId
     ) external onlyOwner {
-        uint256 tokenId = nextTokenId++;
         _mint(winner, tokenId);
-        tournamentIds[tokenId] = tournamentId;
 
         emit TournamentNftMinted(winner, tokenId, tournamentId);
     }
@@ -37,10 +31,10 @@ contract TournamentNft is ERC721, Ownable {
      * @dev Override to prevent any transfer
      */
     function _isAuthorized(
-        address owner,
-        address spender, 
-        uint256 tokenId
+        address /*owner*/,
+        address /*spender*/,
+        uint256 /*tokenId*/
     ) internal view virtual override returns (bool) {
-            return (false);
+        return (false);
     }
 }
