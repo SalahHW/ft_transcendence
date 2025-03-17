8fe3eb8599ff928233d71e6b35b6a1e53030e398
Salah <salah.bouheniche@gmail.com>
2025-03-17 04:38:30 +0100

    feat: Implement Fastify server for users service with environment variable validation

---
+++ users-service/src/app.js
@@ -0,0 +1,30 @@
+import Fastify from 'fastify';
+const fastify = Fastify();
+
+// Process env variables
+const PORT=process.env.USERS_SERVICE_PORT;
+const validEnvironement = PORT;
+
+if(!validEnvironement) {
+  console.error('Unable to load environement variables');
+  process.exit(1);
+}
+
+fastify.route({
+  method: 'GET',
+  url: '/',
+  handler: function(req, res) {
+    res.send({ hello: 'world' });
+  }
+})
+
+fastify.listen({
+  port: 3000,
+  host: '0.0.0.0'
+}, (err, address) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.info(`server listening on ${address}`);
+})
