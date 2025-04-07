a027523255c657ead9d131285836e545cd3ae7d3
joel688 <joachimkoenig68@icloud.com>
2025-04-07 17:23:59 +0200

    fix: update localhost URL in hardhat configuration for consistency

--- pongChain/hardhat.config.cjs
+++ pongChain/hardhat.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
     solidity: "0.8.20",
     networks: {
         localhost: {
-            url: "http://0.0.0.0:3001",
+            url: "http://127.0.0.1:3001",
         },
     },
 };
