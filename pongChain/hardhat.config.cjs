7b3693c179964521ef0d4b32a925683c2764d05d
joel688 <joachimkoenig68@icloud.com>
2025-04-09 15:41:20 +0200

    refactor: update Hardhat configuration to use Fuji network

--- pongChain/hardhat.config.cjs
+++ pongChain/hardhat.config.cjs
@@ -1,10 +1,14 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
     solidity: "0.8.20",
     networks: {
-        localhost: {
-            url: "http://127.0.0.1:3001",
+        fuji: {
+            url: "https://api.avax-test.network/ext/bc/C/rpc",
+            chainId: 43113,
+            accounts: [
+                process.env.PRIVATE_KEY],
         },
     },
 };
