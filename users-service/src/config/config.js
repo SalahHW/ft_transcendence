040c1ba86724e139b65458a29c8fa950c21c58df
Salah <salah.bouheniche@gmail.com>
2025-04-12 01:31:02 +0200

    feat: Export configuration constants for environment setup

--- users-service/src/config/config.js
+++ users-service/src/config/config.js
@@ -1,12 +1,10 @@
-const isDev = process.env.NODE_ENV === 'development';
+export const isDev = process.env.NODE_ENV === "development";
 
-const PORT = isDev ? 3000 : process.env.USERS_SERVICE_PORT;
-const SECRETKEY = isDev ? 'secret key' : process.env.SECRETKEY;
+export const PORT = isDev ? 3000 : process.env.USERS_SERVICE_PORT;
+export const SECRETKEY = isDev ? "secret key" : process.env.SECRETKEY;
 const validEnv = PORT && SECRETKEY;
 
 if (!validEnv) {
-  console.error('Unable to load environement variables');
+  console.error("Unable to load environement variables");
   process.exit(1);
 }
-
-export default { PORT, SECRETKEY, isDev };
