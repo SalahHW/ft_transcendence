0a09236e1dad7cb645b1f7dd98f7f8be07f595d6
Salah <salah.bouheniche@gmail.com>
2025-04-28 13:23:40 +0200

    fix: correct typo in error message for loading secret key

--- jwt-service/src/config/config.js
+++ jwt-service/src/config/config.js
@@ -1,6 +1,6 @@
 export const SECRETKEY = process.env.SECRETKEY;
 
 if (!SECRETKEY) {
-  console.error("Enable to load secret key from evironement variables");
+  console.error("Unable to load secret key from evironement variables");
   process.exit(1);
 }
