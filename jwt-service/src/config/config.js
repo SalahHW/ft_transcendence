3b4eb295f397343def4fb9ad9425f5d8b94fc417
Salah <salah.bouheniche@gmail.com>
2025-05-01 07:41:42 +0200

    fix: correct typo in error message

--- jwt-service/src/config/config.js
+++ jwt-service/src/config/config.js
@@ -2,6 +2,10 @@ export const SECRETKEY = process.env.SECRETKEY;
 export const PORT = process.env.JWT_SERVICE_PORT;
 
 if (!SECRETKEY) {
+  console.error("Unable to load secret key from environement variables");
+  process.exit(1);
+}
+
 if (!PORT) {
   console.error("Unable to load port from environement variables");
   process.exit(1);
