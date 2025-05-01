64acde6a0b65a99e4a75c92b504114452a571b55
Jaqensv <matthieu.dl@hotmail.fr>
2025-05-01 15:29:21 +0200

    feat: implement user controllers CR / D + username R

--- users-service/src/controllers/users/usernameControllers.js
+++ users-service/src/controllers/users/usernameControllers.js
@@ -4,20 +4,38 @@ async function createUsername(request, reply) {
     const { username } = request.body;
 
     if (!username) {
-        return reply.code(400).send({ error: "Username is required"});
+        return reply.code(400).send({ error: "Username is required" });
     }
+    try {
+        const newUsername = await usernameModels.createUsername(username);
+        return reply.code(200).send(newUsername);
+    } catch (err) {
+        return reply.code(500).send({ error: "Failed to create username" });
+    }
+}
 
-    return reply.code(200).send(username);
+async function readUsername(request, reply) {
+    const { userId } = request.body;
+
+    if (!userId) {
+        return reply.code(400).send({ error: "UserId is required" });
+    }
+    try {
+        const username = await usernameModels.readUsername(userId);
+        return reply.code(200).send(username);
+    } catch (err) {
+        return reply.code(500).send({ error: "Failed to read username" });
+    }
 }
 
 async function updateUsername(request, reply) {
     const { username } = request.body;
 
     if (!username) {
-        return reply.code(400).send({ error: "Username is required for the update"});
+        return reply.code(400).send({ error: "Username is required for the update" });
     }
     try {
-        const newUsername = await usernameModels.update(username);
+        const newUsername = await usernameModels.updateUsername(username);
         return reply.code(200).send(newUsername);
     } catch (err) {
         return reply.code(500).send({ error: "Failed to update username" });
