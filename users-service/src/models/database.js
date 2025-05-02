28ee4ffde02fede13798fd8282f1a2197b385c83
Salah <salah.bouheniche@gmail.com>
2025-05-02 11:58:51 +0200

    fix: correct typo in email field definition in users table

--- users-service/src/models/database.js
+++ users-service/src/models/database.js
@@ -17,7 +17,7 @@ export const initializeDatabase = async () => {
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       username TEXT UNIQUE NOT NULL,
       password TEXT NOT NULL,
-      email TEXT UNIQUE NOT NUL,
+      email TEXT UNIQUE NOT NULL,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       );`;
       
