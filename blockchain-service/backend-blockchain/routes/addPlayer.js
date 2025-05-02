5fb6ba87c3f8f6a134bc7d9694814dd512dfb981
joel688 <joachimkoenig68@icloud.com>
2025-05-02 10:35:49 +0200

    refactor: Remove JWT verification from add-player route

--- blockchain-service/backend-blockchain/routes/addPlayer.js
+++ blockchain-service/backend-blockchain/routes/addPlayer.js
@@ -2,7 +2,6 @@ module.exports = async (fastify, opts) => {
     const contract = fastify.masterContract
 
     fastify.post('/add-player', {
-        preHandler: fastify.verifyJWT,
         schema: {
             body: {
                 type: 'object',
