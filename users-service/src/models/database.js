a90aaaba1acc3fecbf8de9f646b9fb1c52827302
Salah <salah.bouheniche@gmail.com>
2025-05-02 16:27:15 +0200

    refactor:  remove commented-out sections

--- users-service/src/models/database.js
+++ users-service/src/models/database.js
@@ -20,7 +20,7 @@ export const initializeDatabase = async () => {
       email TEXT UNIQUE NOT NULL,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       );`;
-      
+
     await database.exec(query);
     console.log("Database initialized");
   } catch (error) {
@@ -28,30 +28,3 @@ export const initializeDatabase = async () => {
     throw error;
   }
 };
-
-// const db = new sqlite3.Database('./database/db.sqlite', (err) => {
-//   if (err) {
-//     throw new Error('Connection error to SQLite: ', err.message);
-//   } else {
-//     console.log('Connected to SQLite');
-//   }
-// });
-
-// const initializeDatabase = () => {
-//   const createUsersTable = `
-//   CREATE TABLE IF NOT EXISTS users (
-//   id INTEGER PRIMARY KEY AUTOINCREMENT,
-//       username TEXT UNIQUE NOT NULL,
-//       password TEXT NOT NULL,
-//       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-//     );
-//   `;
-
-//   db.run(createUsersTable, (err) => {
-//     if (err) {
-//       throw new Error("Error creating users table: ", err.message);
-//     } else {
-//       console.log("Database initialized");
-//     }
-//   });
-// };
