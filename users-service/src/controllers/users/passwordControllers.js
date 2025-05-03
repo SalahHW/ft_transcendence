9205cdd24245c64241902ca686f5f29710435d6a
Jaqensv <matthieu.dl@hotmail.fr>
2025-05-03 19:03:15 +0200

    feat: add passwordControllers

---
+++ users-service/src/controllers/users/passwordControllers.js
@@ -0,0 +1,44 @@
+import * as passwordModels from "../models/userModels/passwordModels.js";
+
+export async function createPassword(request, reply) {
+    const { password } = request.body;
+    const { userId } = request.params.id;
+
+    if (!password) {
+        return reply.code(400).send({ error: "Password is required "});
+    }
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const result = await passwordModels.createPassword(userId, hashedPassword);
+        return reply.code(200).send(result);
+    } catch (error) {
+        return reply.code(500).send({
+            error: "Failed to create the password",
+            cause: error.message,
+        });
+    }
+}
+
+export async function updatePassword(request, reply) {
+    const { oldPassword, newPassword } = request.body;
+    const { user } = request.params;
+    const { userId } = request.params.id;
+
+    if (!oldPassword || !newPassword) {
+        return reply.code(400).send({ error: "Old and new password are required" });
+    }
+    const sameOldPassword = await bcrypt.compare(oldPassword, user.password);
+    if (!sameOldPassword) {
+        return reply.code(403).send({ error: "Old password doesn't match" });
+    }
+    try {
+        const newHashedPassword = await bcrypt.hash(newPassword, 10);
+        const result = await passwordModels.updatePassword(userId, newHashedPassword);
+        return reply.code(200).send(result);
+    } catch (error) {
+        return reply.code(500).send({
+            error: "Failed to update the password",
+            cause: error.message
+        });
+    }
+}
