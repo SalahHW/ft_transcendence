00f4ffd3a7983f32799852d5d1ab0ee517609df7
Jaqensv <matthieu.dl@hotmail.fr>
2025-05-04 16:12:48 +0200

    feat: add readPassword

--- users-service/src/controllers/users/passwordControllers.js
+++ users-service/src/controllers/users/passwordControllers.js
@@ -19,6 +19,23 @@ export async function createPassword(request, reply) {
     }
 }
 
+export async function readPassword(request, reply) {
+    const { userId } = request.params.id;
+
+    if (!userId) {
+        return reply.code(400).send({ error: "UserId required" });
+    }
+    try {
+        const password = await passwordModels.readPassword(userId);
+        return reply.code(200).send(password);
+    } catch (error) {
+        return reply.code(500).send({
+            error: "Failed to read the password",
+            cause: error.message,
+        });
+    }
+}
+
 export async function updatePassword(request, reply) {
     const { oldPassword, newPassword } = request.body;
     const { user } = request.params;
