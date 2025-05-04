534b89608d0e0c75d3c954f2174e710c6a7b0559
Salah <salah.bouheniche@gmail.com>
2025-05-04 17:51:21 +0200

    fix: comment out unused route imports

--- users-service/src/routes/index.js
+++ users-service/src/routes/index.js
@@ -1,11 +1,15 @@
-import getUsersRoutes from "./users/readRoutes.js";
-import registerUsersRoutes from "./auth/register.js";
-import loginRoutes from "./auth/login.js";
-import meRoute from "./auth/me.js";
+// import getUsersRoutes from "./users/readRoutes.js";
+// import registerUsersRoutes from "./auth/register.js";
+// import loginRoutes from "./auth/login.js";
+// import meRoute from "./auth/me.js";
+
+import userRoutes from "./users/userRoutes.js";
 
 export default async function registerRoutes(fastify, options) {
-  fastify.register(getUsersRoutes);
-  fastify.register(registerUsersRoutes);
-  fastify.register(loginRoutes);
-  fastify.register(meRoute);
+  // fastify.register(getUsersRoutes);
+  // fastify.register(registerUsersRoutes);
+  // fastify.register(loginRoutes);
+  // fastify.register(meRoute);
+
+  fastify.register(userRoutes);
 }
