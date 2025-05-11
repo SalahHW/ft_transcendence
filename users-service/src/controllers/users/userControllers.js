dcedba5cdeccd6977200adaa6910256bc320b477
Salah <salah.bouheniche@gmail.com>
2025-05-11 04:12:52 +0200

    fix: Correct object format sent to createUser model

--- users-service/src/controllers/users/userControllers.js
+++ users-service/src/controllers/users/userControllers.js
@@ -6,22 +6,28 @@ import { encryptPassword } from "../../utils/password.js";
 export async function createUser(request, reply) {
   const { username, password, email } = request.body;
 
-    if (!username || !password || !email) {
-        return reply.code(400).send({ error: "Lack of information related to the user" });
-    }
-    try {
-        // TODO: Use username and email controllers to verify username and email
-        // createUsername();
-        // createEmail();
-        const hashedPassword = await encryptPassword(password);
-        const newUser = await userModels.createUser({ username, hashedPassword, email });
-        return reply.code(201).send(newUser);
-    } catch (error) {
-        return reply.code(500).send({
-            error: "Failed to create the user",
-            cause: error.message,
-        });
-    }
+  if (!username || !password || !email) {
+    return reply
+      .code(400)
+      .send({ error: "Lack of information related to the user" });
+  }
+  try {
+    // TODO: Use username and email controllers to verify username and email
+    // createUsername();
+    // createEmail();
+    const hashedPassword = await encryptPassword(password);
+    const newUser = await userModels.createUser({
+      username,
+      password: hashedPassword,
+      email,
+    });
+    return reply.code(201).send(newUser);
+  } catch (error) {
+    return reply.code(500).send({
+      error: "Failed to create the user",
+      cause: error.message,
+    });
+  }
 }
 
 export async function readUser(request, reply) {
@@ -46,18 +52,18 @@ export async function readUser(request, reply) {
 }
 
 export async function readAllUsers(request, reply) {
-    try {
-        const users = await userModels.readAllUsers();
-        if (!users) {
-            return reply.code(404).send({ error: "No users found" });
-        }
-        return reply.code(200).send(users);
-    } catch (error) {
-        return reply.code(500).send({
-            error: "Failed to read all users",
-            cause: error.message,
-        });
+  try {
+    const users = await userModels.readAllUsers();
+    if (!users) {
+      return reply.code(404).send({ error: "No users found" });
     }
+    return reply.code(200).send(users);
+  } catch (error) {
+    return reply.code(500).send({
+      error: "Failed to read all users",
+      cause: error.message,
+    });
+  }
 }
 
 export async function updateUser(request, reply) {
@@ -68,7 +74,11 @@ export async function updateUser(request, reply) {
     return reply.code(400).send({ error: "UserId is required" });
   }
   try {
-    const updatedUser = await userModels.updateUser(userId, { username, password, email });
+    const updatedUser = await userModels.updateUser(userId, {
+      username,
+      password,
+      email,
+    });
     return reply.code(200).send(updatedUser);
   } catch (error) {
     return reply.code(500).send({
@@ -81,16 +91,18 @@ export async function updateUser(request, reply) {
 export async function deleteUser(request, reply) {
   const userId = request.params.id;
 
-	if (!userId) {
-		return reply.code(400).send({ error: "UserId is required" });
-	}
-	try {
-    	const deletedUserId = await userModels.deleteUser(userId);
-    	return reply.code(204).send({ success: `User ${deletedUserId} has been deleted with success` });
-  	} catch (error) {
-    	return reply.code(500).send({
-      		error: "Failed to delete the user",
-      		cause: error.message,
-    	});
-  	}
+  if (!userId) {
+    return reply.code(400).send({ error: "UserId is required" });
+  }
+  try {
+    const deletedUserId = await userModels.deleteUser(userId);
+    return reply
+      .code(204)
+      .send({ success: `User ${deletedUserId} has been deleted with success` });
+  } catch (error) {
+    return reply.code(500).send({
+      error: "Failed to delete the user",
+      cause: error.message,
+    });
+  }
 }
