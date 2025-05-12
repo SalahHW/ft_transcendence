5eb440eb06dccc51c8bba8dff00df761e41c7790
Jaqensv <matthieu.dl@hotmail.fr>
2025-05-12 11:35:53 +0200

    feat: validator library added into createEmail, use of validator method. RawEmail trimmed

--- users-service/src/controllers/users/emailControllers.js
+++ users-service/src/controllers/users/emailControllers.js
@@ -1,52 +1,53 @@
 import * as emailModels from "../../models/userModels/emailModels.js";
+import validator from 'validator';
 
-// export async function createEmail(request, reply) {
-//     const { email } = request.body;
+export async function createEmail(request, reply) {
+	const { rawEmail } = request.body;
 
-//     if (!email) {
-//         return reply.code(400).send({ error: "UserId is required" });
-//     }
-//     try {
-//         const email = await emailModels.createEmail(email);
-//         return reply.code(200).send(email);
-//     } catch (error) {
-//         return reply.code(500).send({
-//             error: "Failed to create an email",
-//             cause: error.message,
-//         });
-//     }
-// }
+	if (typeof rawEmail !== 'string') {
+		throw new Error("Email must be a string");
+	}
+	// Delete whitespaces
+	const email = rawEmail.trim();
+
+	// Norme RFC 5322 simplifiée
+	if (!email || !validator.isEmail(email)) {
+		throw new Error("Invalid email address");
+	}
+	return email;
+}
 
 export async function readEmail(request, reply) {
-    const userId = request.params.id;
+	const userId = request.params.id;
 
-    if (!userId) {
-        return reply.code(400).send({ error: "UserId is required" });
-    }
-    try {
-        const email = await emailModels.readEmail(userId);
-        return reply.code(200).send(email);
-    } catch (error) {
-        return reply.code(500).send({
-            error: "Failed to read the email",
-            cause: error.message,
-        });
-    }
+	if (!userId) {
+		return reply.code(400).send({ error: "UserId is required" });
+	}
+	try {
+		//const clean = validator.escape(validator.stripLow(trimmed));
+		const email = await emailModels.readEmail(userId);
+		return reply.code(200).send(email);
+	} catch (error) {
+		return reply.code(500).send({
+			error: "Failed to read the email",
+			cause: error.message,
+		});
+	}
 }
 
 export async function updateEmail(request, reply) {
-    const { email } = request.body;
+	const { email } = request.body;
 
-    if (!email) {
-        return reply.code(400).send({ error: "Email is required" });
-    }
-    try {
-        const newEmail = await emailModels.updateEmail(email);
-        return reply.code(200).send(newEmail);
-    } catch (error) {
-        return reply.code(500).send({
-            error: "Failed to update the email",
-            cause: error.message,
-        });
-    }
+	if (!email) {
+		return reply.code(400).send({ error: "Email is required" });
+	}
+	try {
+		const newEmail = await emailModels.updateEmail(email);
+		return reply.code(200).send(newEmail);
+	} catch (error) {
+		return reply.code(500).send({
+			error: "Failed to update the email",
+			cause: error.message,
+		});
+	}
 }
