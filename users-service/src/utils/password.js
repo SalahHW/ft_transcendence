e94487aa77df123994ea6f6e0db7e633b08452b1
Jaqensv <matthieu.dl@hotmail.fr>
2025-05-12 13:05:56 +0200

    fix: password throw message fix

--- users-service/src/utils/password.js
+++ users-service/src/utils/password.js
@@ -7,7 +7,7 @@ export async function encryptPassword(password) {
 		// Mot de passe compose de lettres, chiffres et underscores. 6 a 12 caracteres.
 		const regexPattern = /^[a-zA-Z0-9?!.#*"']{6,12}$/;
 		if (!regexPattern.test(password)) {
-				throw new Error("Invalid password format. It must be 6 to 12 characters long and contain only letters, numbers, or underscores.");
+				throw new Error("Invalid password format. It must be 6 to 12 characters long and contain only letters, numbers, or special characters");
 		}
 		return await bcrypt.hash(password, 10);
 }
