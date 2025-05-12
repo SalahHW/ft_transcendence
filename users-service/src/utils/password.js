ba93da6d3a9f5fe73be45a4fef7cc1aa595e8b6c
Jaqensv <matthieu.dl@hotmail.fr>
2025-05-12 12:53:09 +0200

    fix: some special characters added to the regex

--- users-service/src/utils/password.js
+++ users-service/src/utils/password.js
@@ -1,23 +1,23 @@
 import bcrypt from "bcrypt";
 
 export async function encryptPassword(password) {
-    if (!password) {
-        throw new Error("Password is required. Encryption failed");
-    }
-    // Mot de passe compose de lettres, chiffres et underscores. 6 a 12 caracteres.
-    const regexPattern = /^[a-zA-Z0-9_]{6,12}$/;
-    if (!regexPattern.test(password)) {
-        throw new Error("Invalid password format. It must be 6 to 12 characters long and contain only letters, numbers, or underscores.");
-    }
-    return await bcrypt.hash(password, 10);
+		if (!password) {
+				throw new Error("Password is required. Encryption failed");
+		}
+		// Mot de passe compose de lettres, chiffres et underscores. 6 a 12 caracteres.
+		const regexPattern = /^[a-zA-Z0-9?!.#*"']{6,12}$/;
+		if (!regexPattern.test(password)) {
+				throw new Error("Invalid password format. It must be 6 to 12 characters long and contain only letters, numbers, or underscores.");
+		}
+		return await bcrypt.hash(password, 10);
 }
 
 export async function comparePassword(passwordA, passwordB) {
-    // Boolean
-    const isValidPassword = await bcrypt.compare(passwordA, passwordB);
+		// Boolean
+		const isValidPassword = await bcrypt.compare(passwordA, passwordB);
 
-    if (!isValidPassword) {
-        return false;
-    }
-    return true;
+		if (!isValidPassword) {
+				return false;
+		}
+		return true;
 }
