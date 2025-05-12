d0649f14784559ad351291160c63a4bc6ce505f6
Jaqensv <matthieu.dl@hotmail.fr>
2025-05-12 13:09:38 +0200

    fix: encryptPassword refacto

--- users-service/src/utils/password.js
+++ users-service/src/utils/password.js
@@ -1,12 +1,9 @@
 import bcrypt from "bcrypt";
 
 export async function encryptPassword(password) {
-		if (!password) {
-				throw new Error("Password is required. Encryption failed");
-		}
 		// Mot de passe compose de lettres, chiffres et underscores. 6 a 12 caracteres.
 		const regexPattern = /^[a-zA-Z0-9?!.#*"']{6,12}$/;
-		if (!regexPattern.test(password)) {
+		if (!password || !regexPattern.test(password)) {
 				throw new Error("Invalid password format. It must be 6 to 12 characters long and contain only letters, numbers, or special characters");
 		}
 		return await bcrypt.hash(password, 10);
