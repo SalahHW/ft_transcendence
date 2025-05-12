2a1d19d8396b57da9954992d37fc4b29f4496892
Salah <salah.bouheniche@gmail.com>
2025-05-12 18:24:50 +0200

    feat: implement loginUser function to authenticate users with username and password

--- users-service/src/controllers/authenticationControllers.js
+++ users-service/src/controllers/authenticationControllers.js
@@ -1,4 +1,6 @@
 import { createUser } from "./users/userControllers.js";
+import { readUserByUsername } from "../models/userModels/userModels.js";
+import { comparePassword } from "../utils/password.js";
 
 export const registerUser = async (request, reply) => {
   return createUser(request, reply);
@@ -13,5 +15,23 @@ export const loginUser = async (request, reply) => {
     return;
   }
 
-  
+  try {
+    const user = await readUserByUsername(username);
+    const isValidPass = comparePassword(password, user.password);
+    if (!isValidPass) {
+      return reply.code(401).send({ error: "Invalid credentials" });
+    }
+
+    const token = request.server.signToken({
+      sub: user.id,
+      username: user.username,
+      aud: "users-service",
+    });
+
+    reply.code(200).send(token);
+  } catch (error) {
+    return reply
+      .code(500)
+      .send({ error: "Login failed", cause: error.message });
+  }
 };
