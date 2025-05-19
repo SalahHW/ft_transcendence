77e8b5bd89926e2adffcac63b430249d9a3691d4
Salah <salah.bouheniche@gmailcom>
2025-05-19 07:05:47 +0200

    refactor: remove unused login and registration controllers and routes

--- users-service/src/routes/auth/login.js
+++
@@ -1,9 +0,0 @@
-import { loginUser } from "../../controllers/auth/login.js";
-
-export default async function loginRoutes(fastify, options) {
-  fastify.route({
-    method: "POST",
-    url: "/login",
-    handler: loginUser,
-  });
-}
