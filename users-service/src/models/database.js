4088b7954284a39256ef515682d5b6c47bb3f425
Salah <salah.bouheniche@gmail.com>
2025-05-21 03:56:44 +0200

    fix: streamline database path usage and improve error handling in initialization

--- users-service/src/models/database.js
+++ users-service/src/models/database.js
@@ -2,16 +2,15 @@ import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 import { DB_PATH } from "../config/config.js";
 
-const databasePath = DB_PATH;
 export let database;
 
 export const initializeDatabase = async () => {
   try {
     database = await open({
-      filename: databasePath,
+      filename: DB_PATH,
       driver: sqlite3.Database,
     });
-    console.log("Connected to SQLite at ", databasePath);
+    console.log("Connected to SQLite at ", DB_PATH);
 
     const query = `
     CREATE TABLE IF NOT EXISTS users (
@@ -26,6 +25,7 @@ export const initializeDatabase = async () => {
     console.log("Database initialized");
   } catch (error) {
     console.error("Database initialization failed:", error.message);
-    throw error;
+    console.error("File should be located at ", DB_PATH);
+    process.exit(1);
   }
 };
