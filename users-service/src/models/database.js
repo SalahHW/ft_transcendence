9d03cfb11918887e122da7704a5f380c0eba255e
joel688 <joachimkoenig68@icloud.com>
2025-06-02 20:15:12 +0200

    fix: add walletAddress handling in user creation and update functions

--- users-service/src/models/database.js
+++ users-service/src/models/database.js
@@ -1,31 +1,32 @@
-import sqlite3 from "sqlite3";
-import { open } from "sqlite";
-import { DB_PATH } from "../config/config.js";
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import { DB_PATH } from '../config/config.js';
 
 export let database;
 
 export const initializeDatabase = async () => {
-  try {
-    database = await open({
-      filename: DB_PATH,
-      driver: sqlite3.Database,
-    });
-    console.log("Connected to SQLite at ", DB_PATH);
+    try {
+        database = await open({
+            filename: DB_PATH,
+            driver: sqlite3.Database,
+        });
+        console.log('Connected to SQLite at ', DB_PATH);
 
-    const query = `
+        const query = `
     CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       username TEXT UNIQUE NOT NULL,
       password TEXT NOT NULL,
       email TEXT UNIQUE NOT NULL,
+      walletAddress TEXT UNIQUE NOT NULL,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       );`;
 
-    await database.exec(query);
-    console.log("Database initialized");
-  } catch (error) {
-    console.error("Database initialization failed:", error.message);
-    console.error("File should be located at ", DB_PATH);
-    process.exit(1);
-  }
+        await database.exec(query);
+        console.log('Database initialized');
+    } catch (error) {
+        console.error('Database initialization failed:', error.message);
+        console.error('File should be located at ', DB_PATH);
+        process.exit(1);
+    }
 };
