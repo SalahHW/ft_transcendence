6bcad66658c8f5b3f19669387ebcc716e2cabc2c
SalahHW <salah.bouheniche@gmail.com>
2025-06-22 17:03:31 +0200

    feat: add avatar controller functions for CRUD operations

---
+++ avatars-service/src/controllers/avatarControllers.js
@@ -0,0 +1,17 @@
+import * as avatarModels from "../models/avatarModels.js";
+
+export const createAvatar = async (request, reply) => {
+
+}
+
+export const readAvatar = async (request, reply) => {
+
+}
+
+export const updateAvatar = async (request, reply) => {
+
+}
+
+export const deleteAvatar = async (request, reply) => {
+
+}
