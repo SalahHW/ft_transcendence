0984232535f7b2ef25428745a7753b947271768d
SalahHW <salah.bouheniche@gmail.com>
2025-06-23 12:09:55 +0200

    feat: register multipart plugin with file upload limits in app.js

--- avatars-service/src/app.js
+++ avatars-service/src/app.js
@@ -1,9 +1,24 @@
 import { PORT, isDev } from "./config/config.js";
 import Fastify from "fastify";
+import Multipart from "@fastify/multipart";
 import { initializeDatabase } from "./models/database.js";
-// import registerRoutes from "./routes/index.js";
+import registerRoutes from "./routes/index.js";
 
 const fastify = Fastify();
+
+await fastify.register(Multipart, {
+  limits: {
+    limits: {
+      fieldNameSize: 100, // Max field name size in bytes
+      fieldSize: 100, // Max field value size in bytes
+      fields: 10, // Max number of non-file fields
+      fileSize: 10 * 1024 * 1024, // For multipart forms, the max file size in bytes (10 MB)
+      files: 1, // Max number of file fields
+      headerPairs: 2000, // Max number of header key=>value pairs
+      parts: 1000, // For multipart forms, the max number of parts (fields + files)
+    },
+  },
+});
 
 // Initialize the database
 try {
@@ -12,6 +27,8 @@ try {
   console.error("Failed to initialize the database: ", error.message);
   process.exit(1);
 }
+
+fastify.register(registerRoutes);
 
 fastify.listen(
   {
