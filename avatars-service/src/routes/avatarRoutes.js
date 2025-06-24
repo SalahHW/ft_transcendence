e0999be924b9de28cd87f9b82c243cf36dc8b75e
SalahHW <salah.bouheniche@gmail.com>
2025-06-24 07:49:28 +0200

    feat: add PUT and DELETE routes

--- avatars-service/src/routes/avatarRoutes.js
+++ avatars-service/src/routes/avatarRoutes.js
@@ -4,7 +4,6 @@ export default async function avatarRoutes(fastify) {
   fastify.route({
     method: "POST",
     url: "/avatars/id/:id",
-    // preHandler: fastify.multipart,
     handler: avatarControllers.createAvatar,
   });
 
@@ -12,5 +11,17 @@ export default async function avatarRoutes(fastify) {
     method: "GET",
     url: "/avatars/id/:id",
     handler: avatarControllers.readAvatar,
+  });
+
+  fastify.route({
+    method: "PUT",
+    url: "/avatars/id/:id",
+    handler: avatarControllers.updateAvatar,
+  });
+
+  fastify.route({
+    method: "DELETE",
+    url: "/avatars/id/:id",
+    handler: avatarControllers.deleteAvatar,
   });
 }
