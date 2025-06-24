38f6ab7a741dbbaf772f69c2f687dce7a7566e73
SalahHW <salah.bouheniche@gmail.com>
2025-06-24 06:52:10 +0200

    refactor: restructure main function for better readability and flow

--- avatars-service/src/app.js
+++ avatars-service/src/app.js
@@ -6,38 +6,39 @@ import registerRoutes from "./routes/index.js";
 
 const fastify = Fastify();
 
-await fastify.register(Multipart, {
-  limits: {
-    fieldNameSize: 100, // Max field name size in bytes
-    fieldSize: 100, // Max field value size in bytes
-    fields: 10, // Max number of non-file fields
-    fileSize: 10 * 1024 * 1024, // For multipart forms, the max file size in bytes (10 MB)
-    files: 1, // Max number of file fields
-    headerPairs: 2000, // Max number of header key=>value pairs
-    parts: 1000, // For multipart forms, the max number of parts (fields + files)
-  },
-});
+async function main() {
+  await fastify.register(Multipart, {
+    limits: {
+      fieldNameSize: 100, // Max field name size in bytes
+      fieldSize: 100, // Max field value size in bytes
+      fields: 10, // Max number of non-file fields
+      fileSize: 10 * 1024 * 1024, // For multipart forms, the max file size in bytes (10 MB)
+      files: 1, // Max number of file fields
+      headerPairs: 2000, // Max number of header key=>value pairs
+      parts: 1000, // For multipart forms, the max number of parts (fields + files)
+    },
+  });
 
-// Initialize the database
-try {
-  await initializeDatabase();
-} catch (error) {
-  console.error("Failed to initialize the database: ", error.message);
-  process.exit(1);
+  // Initialize the database
+  try {
+    await initializeDatabase();
+  } catch (error) {
+    console.error("Failed to initialize the database: ", error.message);
+    process.exit(1);
+  }
+
+  await fastify.register(registerRoutes);
+
+  try {
+    const address = fastify.listen({
+      port: PORT,
+      host: "0.0.0.0",
+    });
+    fastify.log.info(`server listening on ${address}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 }
 
-fastify.register(registerRoutes);
-
-fastify.listen(
-  {
-    port: PORT,
-    host: "0.0.0.0",
-  },
-  (err, address) => {
-    if (err) {
-      fastify.log.error(err);
-      process.exit(1);
-    }
-    fastify.log.info(`server listening on ${address}`);
-  }
-);
+main();
