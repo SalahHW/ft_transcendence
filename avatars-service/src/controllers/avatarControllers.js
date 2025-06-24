b97109a564fa387aeff9648b338c27de50ad54ae
SalahHW <salah.bouheniche@gmail.com>
2025-06-24 05:03:59 +0200

    feat: implement avatar upload functionality with multipart handling

--- avatars-service/src/controllers/avatarControllers.js
+++ avatars-service/src/controllers/avatarControllers.js
@@ -1,17 +1,43 @@
 import * as avatarModels from "../models/avatarModels.js";
+import { saveUploadedAvatar } from "../utils/uploadUtils.js";
 
-export const createAvatar = async (request, reply) => {
+export const uploadAvatar = async (request, reply) => {
+  try {
+    if (!request.isMultipart()) {
+      return reply
+        .code(406)
+        .send({ error: "Request is not multipart/form-data" });
+    }
 
-}
+    const fileData = await request.file();
 
-export const readAvatar = async (request, reply) => {
+    if (!fileData) {
+      return reply.code(400).send({ error: "No file uploaded" });
+    }
 
-}
+    const { relativePath } = await saveUploadedAvatar(fileData);
 
-export const updateAvatar = async (request, reply) => {
+    console.log(relativePath);
 
-}
+    const userId = request.params.id;
 
-export const deleteAvatar = async (request, reply) => {
+    await avatarModels.createAvatar(userId, relativePath);
 
-}
+    return reply.code(201).send({
+      message: "Avatar uploaded successfully",
+      avatarUrl: relativePath,
+    });
+  } catch (err) {
+    return reply.code(400).send({
+      error: err.message,
+    });
+  }
+};
+
+export const createAvatar = async (request, reply) => {};
+
+export const readAvatar = async (request, reply) => {};
+
+export const updateAvatar = async (request, reply) => {};
+
+export const deleteAvatar = async (request, reply) => {};
