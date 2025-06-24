7dc3b06376037b8f663aea9976a5e497a03ee834
SalahHW <salah.bouheniche@gmail.com>
2025-06-24 05:03:42 +0200

    fix: remove redundant limits object in multipart registration

--- avatars-service/src/app.js
+++ avatars-service/src/app.js
@@ -8,15 +8,13 @@ const fastify = Fastify();
 
 await fastify.register(Multipart, {
   limits: {
-    limits: {
-      fieldNameSize: 100, // Max field name size in bytes
-      fieldSize: 100, // Max field value size in bytes
-      fields: 10, // Max number of non-file fields
-      fileSize: 10 * 1024 * 1024, // For multipart forms, the max file size in bytes (10 MB)
-      files: 1, // Max number of file fields
-      headerPairs: 2000, // Max number of header key=>value pairs
-      parts: 1000, // For multipart forms, the max number of parts (fields + files)
-    },
+    fieldNameSize: 100, // Max field name size in bytes
+    fieldSize: 100, // Max field value size in bytes
+    fields: 10, // Max number of non-file fields
+    fileSize: 10 * 1024 * 1024, // For multipart forms, the max file size in bytes (10 MB)
+    files: 1, // Max number of file fields
+    headerPairs: 2000, // Max number of header key=>value pairs
+    parts: 1000, // For multipart forms, the max number of parts (fields + files)
   },
 });
 
