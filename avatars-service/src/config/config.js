02f92d82ecb361c7290822ce543c98b2d84bdff2
SalahHW <salah.bouheniche@gmail.com>
2025-06-24 05:10:39 +0200

    feat: update development configuration for avatar upload directory

--- avatars-service/src/config/config.js
+++ avatars-service/src/config/config.js
@@ -1,10 +1,12 @@
 export const isDev = process.env.NODE_ENV === "dev";
 
-const devPort = 3002;
+const devPort = 3001;
 const devDBPath = "./database/avatar.db";
+const devAvatarDir = "./avatars";
 
 export const PORT = isDev ? devPort : process.env.AVATARS_SERVICE_PORT;
 export const DB_PATH = isDev ? devDBPath : process.env.AVATARS_DB_PATH;
+export const AVATAR_UPLOAD_DIR = isDev ? devAvatarDir : process.env.AVATARS_DIR;
 
 if (!PORT) {
   console.error("Unable to load port from environment variables");
