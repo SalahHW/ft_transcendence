acb096f17dd57eda66cf5546bf4d4d83067a60c3
joel688 <joachimkoenig68@icloud.com>
2025-06-26 16:27:47 +0200

    fix: update email queries to reference credentials_auth table

--- users-service/src/models/emailModels.js
+++ users-service/src/models/emailModels.js
@@ -2,18 +2,17 @@ import { database } from "./database.js";
 
 export const readEmail = async (id) => {
   const query = `
-  SELECT email
-  FROM users
-  WHERE id = ?`;
-  const result = await database.get(query, [id]);
-  return result;
+    SELECT email
+    FROM credentials_auth
+    WHERE id = ?`;
+  return await database.get(query, [id]);
 };
 
 export const updateEmail = async (id, newEmail) => {
   const query = `
-  UPDATE users
-  SET email = ?
-  WHERE id = ?`;
-  const result = await database.run(query, [newEmail, id]);
+    UPDATE credentials_auth
+    SET email = ?
+    WHERE id = ?`;
+  const result = await database.run(query, [newEmail.toLowerCase(), id]);
   return result.changes;
 };
