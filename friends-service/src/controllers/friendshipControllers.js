f91beb35a463d94d8900a5cd4f61ffabac36de0d
SalahHW <salah.bouheniche@gmail.com>
2025-06-28 01:37:30 +0200

    feat: implement readFriendship and deleteFriendship functions for managing friendships

--- friends-service/src/controllers/friendshipControllers.js
+++ friends-service/src/controllers/friendshipControllers.js
@@ -22,3 +22,45 @@ export async function createFriendship(request, reply) {
     return sendControllerError(reply, err);
   }
 }
+
+export async function readFriendship(request, reply) {
+  try {
+    const userId = request.params.userId;
+
+    const friendships = await friendshipModels.readFriendship(userId);
+
+    if (friendships.length === 0) {
+      return reply.code(404).send({
+        message: "No friendships found",
+      });
+    }
+
+    return reply.code(200).send(friendships);
+  } catch (err) {
+    return reply.code(500).send({
+      error: "Failed to read friendships",
+      cause: err.message,
+    });
+  }
+}
+
+export async function deleteFriendship(request, reply) {
+  try {
+    const userId = request.params.userId;
+    const friendId = request.params.friendId;
+
+    if (userId === friendId) {
+      return reply
+        .code(400)
+        .send({ error: "User cannot add themselves as a friend" });
+    }
+
+    await friendshipModels.deleteFriendship(userId, friendId);
+    reply.code(204).send();
+  } catch (err) {
+    return reply.code(500).send({
+      error: "Failed to delete friendships",
+      cause: err.message,
+    });
+  }
+}
