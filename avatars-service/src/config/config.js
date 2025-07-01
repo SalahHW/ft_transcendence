d59d46800de24220ab1cca30c35869c5b685d62e
SalahHW <salah.bouheniche@gmail.com>
2025-07-01 16:59:34 +0200

    fix: update environment variable check for development mode and add users service URL

--- avatars-service/src/config/config.js
+++ avatars-service/src/config/config.js
@@ -1,12 +1,19 @@
-export const isDev = process.env.NODE_ENV === "dev";
+export const isDev = process.env.NODE_ENV === "development";
 
 const devPort = 3001;
 const devDBPath = "./database/avatars.db";
 const devAvatarDir = "./avatars";
+const devUsersServiceUrl = "http://localhost:3000";
 
 export const PORT = isDev ? devPort : process.env.AVATARS_SERVICE_PORT;
 export const DB_PATH = isDev ? devDBPath : process.env.AVATARS_DB_PATH;
 export const AVATARS_PATH = isDev ? devAvatarDir : process.env.AVATARS_PATH;
+
+export const USERS_SERVICE_URL = isDev
+  ? devUsersServiceUrl
+  : process.env.USERS_SERVICE_URL;
+
+export const USERS_SERVICE_TIMEOUT = 2000;
 
 if (!PORT) {
   console.error("Unable to load port from environment variables");
@@ -20,4 +27,10 @@ if (!DB_PATH) {
 
 if (!AVATARS_PATH) {
   console.error("Unable to load avatar path from environment variables");
+  process.exit(1);
+}
+
+if (!USERS_SERVICE_URL) {
+  console.error("Unable to load users service url from environment variable");
+  process.exit(1);
 }
