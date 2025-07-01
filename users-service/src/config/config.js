9596fa30761f7dbc43cba5d6d4b203fa1225a44b
SalahHW <salah.bouheniche@gmail.com>
2025-07-01 15:23:06 +0200

    refactor: update development database path in config

--- users-service/src/config/config.js
+++ users-service/src/config/config.js
@@ -1,7 +1,7 @@
 export const isDev = process.env.NODE_ENV === "development";
 
 const devPort = 3000;
-const devDBPath = "./database/db.sqlite";
+const devDBPath = "./database/users.db";
 
 export const PORT = isDev ? devPort : process.env.USERS_SERVICE_PORT;
 export const DB_PATH = isDev ? devDBPath : process.env.DB_PATH;
