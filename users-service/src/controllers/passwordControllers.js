8d5008fe7f7e9c4eecca09fa82c8a8c0cecbf816
Jaqensv <matthieu.dl@hotmail.fr>
2025-07-02 17:18:44 +0200

    feat: presences service integration first push

--- users-service/src/controllers/passwordControllers.js
+++ users-service/src/controllers/passwordControllers.js
@@ -3,10 +3,10 @@ import { readUser } from "./userControllers.js";
 import { encryptPassword, comparePassword } from "../utils/password.js";
 
 export async function createPassword(password) {
-  const regexPattern = /^[a-zA-Z0-9?!.#*"']{6,12}$/;
+  const regexPattern = /^[a-zA-Z0-9?!.#*"']{6,20}$/;
   if (!password || !regexPattern.test(password)) {
     throw new Error(
-      "Invalid password format. It must be 6 to 12 characters long and contain only letters, numbers, or special characters"
+      "Invalid password format. It must be 6 to 20 characters long and contain only letters, numbers, or special characters"
     );
   }
   return await encryptPassword(password);
