45b0741ba0ce73ae4830239ee9a9335c871e9108
Jaqensv <matthieu.dl@hotmail.fr>
2025-07-05 01:06:01 +0200

    feat: add, remove and get online users body

--- presences-service/src/methodes/presencesMethodes.js
+++ presences-service/src/methodes/presencesMethodes.js
@@ -1,7 +1,40 @@
-import onlinePlayers from "../config.js";
+import onlineUsers from "../config.js";
 
-export async function addOnlinePlayer(request) {
-    const { userId } = request.params.id;
+export function addOnlineUser(request, reply) {
+	const { userId } = request.params;
 
-    onlinePlayers.push(userId);
+	if (!userId) {
+		return reply
+			.code(400)
+			.send({ error: "User ID needed" });
+	}
+	onlineUsers.push(userId);
+	return reply
+		.code(201)
+		.send('User ${userId} added to the online list');
+}
+
+export function removeOnlineUser(request, reply) {
+	const { userId } = request.params;
+
+	if (!userId) {
+		return reply
+			.code(400)
+			.send({ error: "User ID needed" });
+	}
+		const index = onlineUsers.indexOf(userId);
+		if (index !== -1) {
+			onlineUsers.splice(index, 1);
+			return reply
+				.code(200)
+				.send('User ${userId} removed from the online list');
+		} else {
+			return reply
+				.code(404)
+				.send({ error: 'User ${userId} not found' });
+		}
+}
+
+export function getOnlineUsers(request, reply) {
+	return reply.code(200).send(onlineUsers);
 }
