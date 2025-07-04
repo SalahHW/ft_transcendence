916e9f923792b3798b8b69ab54de69c98c5660d9
Elias <elias.delarbre@gmail.com>
2025-07-04 14:35:58 +0200

    feat: add Tailwind CSS configuration file for styling

---
+++ nginx/tailwind.config.js
@@ -0,0 +1,13 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"JetBrains Mono"', 'sans-serif'],
+        mono: ['"JetBrains Mono"', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
