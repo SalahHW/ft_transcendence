8172acdd7ae5d23a40e0f151ecddca19c2637111
Jaqensv <matthieu.dl@hotmail.fr>
2025-07-05 01:04:19 +0200

    fix: change onlinePlayers to onlineUsers

--- presences-service/src/config/config.js
+++ presences-service/src/config/config.js
@@ -1,14 +1,14 @@
 import fastify from "fastify";
 
-const fastify = Fastify();
+const fastify = fastify();
 
-let onlinePlayers = []; 
+let onlineUsers = []; 
 
-export default onlinePlayers;
+export default onlineUsers;
 
-faire routes avec methodes add et delete dans tableau
- fichier routes
- fichier methodes
- fichier fastify
+// faire routes avec methodes add et delete dans tableau
+//  fichier routes
+//  fichier methodes
+//  fichier fastify
 
-faire un get pour renvoyer liste des id des players dans le tab, renvoyer json tableau de int
+// faire un get pour renvoyer liste des id des players dans le tab, renvoyer json tableau de int
