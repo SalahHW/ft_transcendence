be104a530fb5e320dc7a650d52e2deda8db26be1
Elias <elias.delarbre@gmail.com>
2025-07-04 16:24:47 +0200

    fix: update font links and Tailwind CSS configuration for consistency and improved styling

--- nginx/tailwind.config.js
+++ nginx/tailwind.config.js
@@ -1,13 +1,14 @@
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['"JetBrains Mono"', 'sans-serif'],
-        mono: ['"JetBrains Mono"', 'monospace'],
-      },
-    },
-  },
-  plugins: [],
-}
+export const theme = {
+	extend: {
+		fontFamily: {
+			sans: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
+			mono: ['Source Code Pro', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+		},
+		colors: {
+			'custom-dark': '#313131',
+			'custom-gray': '#5A5A5A',
+		},
+	},
+};
+export const plugins = [];
