08e16d669ea0b7959aac96dc49664f9c25848b0a
SalahHW <salah.bouheniche@gmail.com>
2025-07-04 02:17:21 +0200

    refactor: streamline environment variable handling for production and development

--- avatars-service/src/config/config.js
+++ avatars-service/src/config/config.js
@@ -1,19 +1,12 @@
 export const isDev = process.env.NODE_ENV === "development";
 
-const devPort = 3001;
-const devDBPath = "./database/avatars.db";
-const devAvatarDir = "./avatars";
-const devUsersServiceUrl = "http://localhost:3000";
-
-export const PORT = isDev ? devPort : process.env.AVATARS_SERVICE_PORT;
-export const DB_PATH = isDev ? devDBPath : process.env.AVATARS_DB_PATH;
-export const AVATARS_PATH = isDev ? devAvatarDir : process.env.AVATARS_PATH;
-
-export const USERS_SERVICE_URL = isDev
-  ? devUsersServiceUrl
-  : process.env.USERS_SERVICE_URL;
-
-export const USERS_SERVICE_TIMEOUT = 2000;
+export const PORT = process.env.AVATARS_SERVICE_PORT;
+export const DB_PATH = isDev
+  ? process.env.DEV_DB_PATH
+  : process.env.AVATARS_DB_PATH;
+export const AVATARS_PATH = process.env.AVATARS_PATH;
+export const USERS_SERVICE_URL = process.env.USERS_SERVICE_URL;
+export const USERS_SERVICE_TIMEOUT = process.env.USERS_SERVICE_TIMEOUT;
 
 if (!PORT) {
   console.error("Unable to load port from environment variables");
@@ -32,5 +25,12 @@ if (!AVATARS_PATH) {
 
 if (!USERS_SERVICE_URL) {
   console.error("Unable to load users service url from environment variable");
+  process.exit(1);
+}
+
+if (!USERS_SERVICE_TIMEOUT) {
+  console.error(
+    "Unable to load users service timeout from environment variable"
+  );
   process.exit(1);
 }
