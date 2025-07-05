fb7ab3dd333f2d1d3b934c00e53668ea73bb1480
Jaqensv <matthieu.dl@hotmail.fr>
2025-07-05 19:24:29 +0200

    fix: add the right urls

--- presences-service/src/routes/presencesRoutes.js
+++ presences-service/src/routes/presencesRoutes.js
@@ -3,19 +3,19 @@ import * as presencesMethodes from "../methodes/presencesMethodes.js";
 export default async function presencesRoutes(fastify) {
   fastify.route({
     method: "POST",
-    url: "/avatars/id/:id",
-    handler: avatarControllers.createAvatar,
+    url: "/presences/id/:id",
+    handler: presencesMethodes.addOnlineUser,
   });
 
   fastify.route({
     method: "GET",
-    url: "/avatars/id/:id",
-    handler: avatarControllers.readAvatar,
+    url: "/presences/id/:id",
+    handler: presencesMethodes.getOnlineUsers,
   });
 
   fastify.route({
     method: "DELETE",
-    url: "/avatars/id/:id",
-    handler: avatarControllers.deleteAvatar,
+    url: "/presences/id/:id",
+    handler: presencesMethodes.removeOnlineUser,
   });
 }
