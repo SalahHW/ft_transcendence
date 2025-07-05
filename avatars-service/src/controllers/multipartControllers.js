ab8745c60d6f4be443da1a72b46f007c7f3b6b74
SalahHW <salah.bouheniche@gmail.com>
2025-07-05 07:44:05 +0200

    fix: remove bug where the stream was already consumed on file verification

--- avatars-service/src/controllers/multipartControllers.js
+++ avatars-service/src/controllers/multipartControllers.js
@@ -1,36 +1,27 @@
 import { httpError } from "../errors/httpErrors.js";
 
-async function getMultipartFile(request) {
+export async function extractFile(request) {
   if (!request.isMultipart()) {
     throw httpError("Request is not multipart/form-data", 406);
   }
 
-  const file = await request.file();
+  const parts = request.parts();
+  let file = null;
+
+  for await (const part of parts) {
+    if (part.file) {
+      if (file) {
+        throw httpError("Only one file allowed per request", 400);
+      }
+      file = part;
+    } else {
+      continue;
+    }
+  }
+
   if (!file) {
     throw httpError("No file uploaded", 400);
   }
 
   return file;
-}
-
-async function ensureSingleFileUpload(request) {
-  const parts = request.parts();
-  let fileCount = 0;
-
-  for await (const part of parts) {
-    if (part.file) {
-      fileCount++;
-    } else {
-      continue;
-    }
-
-    if (fileCount > 1) {
-      throw httpError("Only one file allowed per request", 400);
-    }
-  }
-}
-
-export async function extractFile(request) {
-  await ensureSingleFileUpload(request);
-  return await getMultipartFile(request);
 }
