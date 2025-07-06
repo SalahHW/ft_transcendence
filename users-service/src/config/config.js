114896aeab97a79e06e12a83dd2f68b43c54d0e6
SalahHW <salah.bouheniche@gmail.com>
2025-07-06 14:05:47 +0200

    feat: add Redis configuration and validation to environment variables

--- users-service/src/config/config.js
+++ users-service/src/config/config.js
@@ -1,9 +1,14 @@
 export const isDev = process.env.NODE_ENV === "development";
 
 export const PORT = process.env.USERS_SERVICE_PORT;
-export const DB_PATH = isDev 
-? process.env.DEV_DB_PATH 
-: process.env.USERS_DB_PATH;
+export const DB_PATH = isDev
+  ? process.env.DEV_DB_PATH
+  : process.env.USERS_DB_PATH;
+
+// Redis service
+export const REDIS_HOST = process.env.REDIS_HOST;
+export const REDIS_PORT = process.env.REDIS_PORT;
+export const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
 
 if (!PORT) {
   console.error("Unable to load port from environment variables");
@@ -12,5 +17,18 @@ if (!PORT) {
 
 if (!DB_PATH) {
   console.error("Unable to load database path from environment variables");
+  process.exit(1);
+}
+
+if (!REDIS_HOST) {
+  console.error("Unable to load redis host from environmet variables");
+  process.exit(1);
+}
+if (!REDIS_PORT) {
+  console.error("Unable to load redis port from environmet variables");
+  process.exit(1);
+}
+if (!REDIS_PASSWORD) {
+  console.error("Unable to load redis password from environmet variables");
   process.exit(1);
 }
