8131fa9022376837a5bd1f15de435c7e212b3e30
SalahHW <salah.bouheniche@gmail.com>
2025-07-08 13:36:47 +0200

    feat: add Redis streams and events configuration to config.js

--- users-service/src/config/config.js
+++ users-service/src/config/config.js
@@ -10,6 +10,14 @@ export const REDIS_HOST = process.env.REDIS_SERVICE_HOST;
 export const REDIS_PORT = process.env.REDIS_SERVICE_PORT;
 export const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
 
+export const REDIS_STREAMS = {
+  USERS: process.env.USERS_SERVICE_STREAM,
+};
+
+export const REDIS_EVENTS = {
+  USER_DELETED: process.env.USER_DELETED_EVENT,
+};
+
 if (!PORT) {
   console.error("Unable to load port from environment variables");
   process.exit(1);
@@ -30,5 +38,15 @@ if (!REDIS_PORT) {
 }
 if (!REDIS_PASSWORD) {
   console.error("Unable to load redis password from environmet variables");
+  process.exit(1);
+}
+
+if (!REDIS_STREAMS.USERS) {
+  console.error("Unable to load users service stream from environment variables");
+  process.exit(1);
+}
+
+if (!REDIS_EVENTS.USER_DELETED) {
+  console.error("Unable to load user deleted event from environment variables");
   process.exit(1);
 }
