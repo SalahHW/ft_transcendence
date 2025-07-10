5a30d359b50619d8e6fb62d1e424efb7aac23d52
Elias <elias.delarbre@gmail.com>
2025-07-10 14:25:44 +0200

    fix: correct navigation logic in Wheel component to ensure proper routing based on user login status

--- nginx/src/components/Wheel.ts
+++ nginx/src/components/Wheel.ts
@@ -6,7 +6,7 @@
 /*   By: edelarbr <edelarbr@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 16:41:12 by edelarbr          #+#    #+#             */
-/*   Updated: 2025/07/09 21:09:37 by edelarbr         ###   ########.fr       */
+/*   Updated: 2025/07/10 14:24:00 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,6 @@ export default class Wheel {
       onClick: () => {
         console.log("API Test page clicked");
         this._router.navigate("/api-test");
-        this._router.navigate("/api-test");
       },
     },
     {
@@ -52,7 +51,7 @@ export default class Wheel {
       onClick: () => {
         this._router.navigate("/profile");
       },
-      condition: () => !this._userIsLoggedIn,
+      condition: () => this._userIsLoggedIn,
     },
     {
       label: "Login",
