a56e715ba9b9f8339cba85fb754ce520fac7a49f
SickestOne <richard.vandenreysen@hotmail.com>
2025-07-11 13:30:15 +0200

    fix: game-buttons

---
+++ nginx/src/game/index.ts
@@ -0,0 +1,6 @@
+// Export game client functions
+export { initializeGame, cleanup, leaveGame, setupJoinGameButton } from './client/client.js';
+
+// Export game mode handlers
+export { handleSimpleMatch } from './gameMode/1v1Handler.js';
+export { handleTournament } from './gameMode/tournament/TournamentHandler.js'; 
