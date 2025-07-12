110993e0e69dd04334891a137e96efc32a0d798c
SalahHW <salah.bouheniche@gmail.com>
2025-07-13 01:32:02 +0200

    feat: register email routes in the main routing file

--- users-service/src/routes/index.js
+++ users-service/src/routes/index.js
@@ -1,9 +1,11 @@
 import userRoutes from "./userRoutes.js";
 import authenticationRoutes from "./authenticationRoutes.js";
 import walletAuthenticationRoutes from "./walletAuthenticationRoutes.js";
+import emailRoutes from "./emailRoutes.js";
 
 export default async function registerRoutes(fastify, options) {
   fastify.register(userRoutes);
   fastify.register(authenticationRoutes);
   fastify.register(walletAuthenticationRoutes);
+  fastify.register(emailRoutes);
 }
