edf097d624d20e484604cb2f0fb0806f1c97e2c2
SalahHW <salah.bouheniche@gmail.com>
2025-07-13 22:28:22 +0200

    refactor: comment out userId checks and friendship creation logic in createFriendship function

--- friends-service/src/controllers/friendshipControllers.js
+++ friends-service/src/controllers/friendshipControllers.js
@@ -4,20 +4,21 @@ import { sendControllerError } from "./errors/controllerErrorHandler.js";
 
 export async function createFriendship(request, reply) {
   try {
-    const userId = request.params.userId;
     const friendId = request.params.friendId;
 
-    if (userId === friendId) {
-      return reply
-        .code(400)
-        .send({ error: "User cannot add themselves as a friend" });
-    }
+    console.log("hello", friendId);
 
-    await userServices.userExists(userId);
-    await userServices.userExists(friendId);
+    // if (userId === friendId) {
+    //   return reply
+    //     .code(400)
+    //     .send({ error: "User cannot add themselves as a friend" });
+    // }
 
-    await friendshipModels.createFriendship(userId, friendId);
-    return reply.code(201).send({ message: "Friendship created" });
+    // await userServices.userExists(userId);
+    // await userServices.userExists(friendId);
+
+    // await friendshipModels.createFriendship(userId, friendId);
+    // return reply.code(201).send({ message: "Friendship created" });
   } catch (err) {
     return sendControllerError(reply, err);
   }
