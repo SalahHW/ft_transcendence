0f912f84c7c5ce9e43bede92f7e4d49f1bfe2230
SalahHW <salah.bouheniche@gmail.com>
2025-07-14 15:34:15 +0200

    feat: update preHandler to include user existence verification in friendship route

--- friends-service/src/routes/friendshipRoutes.js
+++ friends-service/src/routes/friendshipRoutes.js
@@ -1,13 +1,17 @@
-import * as friendshipControllers from "../controllers/friendshipControllers.js";
-import * as jwtControllers from "../controllers/jwtControllers.js"
 import * as friendshipSchemas from "../schemas/friendshipSchemas.js";
+import * as friendshipControllers from "../controllers/friendshipControllers.js";
+import * as jwtControllers from "../controllers/jwtControllers.js";
+import * as userControllers from "../controllers/userControllers.js";
 
 export default async function friendshipRoutes(fastify) {
   fastify.route({
     method: "POST",
     url: "/friends/id/:friendId",
     schema: friendshipSchemas.createFriendship,
-    preHandler: jwtControllers.verifyAuthentication,
+    preHandler: [
+      jwtControllers.verifyAuthentication,
+      userControllers.verifyTargetUserExists
+    ],
     handler: friendshipControllers.createFriendship,
   });
 
