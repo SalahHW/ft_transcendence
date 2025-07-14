82069f60f2f4e389db26ae19a9822e0d12f00247
SalahHW <salah.bouheniche@gmail.com>
2025-07-14 15:58:56 +0200

    refactor: streamline createFriendship function by removing commented code and enhancing readability

--- friends-service/src/controllers/friendshipControllers.js
+++ friends-service/src/controllers/friendshipControllers.js
@@ -1,26 +1,24 @@
 import * as friendshipModels from "../models/friendshipModels.js";
-import * as userServices from "../services/userServices.js";
-import { sendControllerError } from "./errors/controllerErrorHandler.js";
 
 export async function createFriendship(request, reply) {
+  const friendId = request.params.friendId;
+  const userId = request.user.sub;
+
+  if (userId === friendId) {
+    return reply
+      .code(400)
+      .send({ error: "User cannot add themselves as a friend" });
+  }
   try {
-    const friendId = request.params.friendId;
-
-    console.log("hello", friendId);
-
-    // if (userId === friendId) {
-    //   return reply
-    //     .code(400)
-    //     .send({ error: "User cannot add themselves as a friend" });
-    // }
-
-    // await userServices.userExists(userId);
-    // await userServices.userExists(friendId);
-
-    // await friendshipModels.createFriendship(userId, friendId);
-    // return reply.code(201).send({ message: "Friendship created" });
+    await friendshipModels.createFriendship(userId, friendId);
+    return reply.code(201).send({ message: "Friendship created" });
   } catch (err) {
-    return sendControllerError(reply, err);
+    if (err.message && err.message.includes("already exists")) {
+      return reply.code(200).send({ message: "Friendship unchanged" });
+    }
+    return reply.code(500).send({
+      error: "Failed to create friendships",
+    });
   }
 }
 
