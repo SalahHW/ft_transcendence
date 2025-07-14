6b1524c1bda3bee2366bee9f79adf5f793d27b58
SalahHW <salah.bouheniche@gmail.com>
2025-07-14 23:50:34 +0200

    fix: correct import statement for usernameSchemas in usernameRoutes

--- users-service/src/routes/usernameRoutes.js
+++ users-service/src/routes/usernameRoutes.js
@@ -1,13 +1,12 @@
 import * as usernameControllers from "../controllers/usernameControllers.js";
-import * as usernameSchema from "../schemas/usernameSchemas.js";
+import * as usernameSchemas from "../schemas/usernameSchemas.js";
 import * as meControllers from "../controllers/meControllers.js";
-import { updateUsername } from "../schemas/usernameSchemas.js";
 
 export default async function usernameRoutes(fastify) {
   fastify.route({
     method: "PUT",
     url: "/users/username",
-    schema: updateUsername,
+    schema: usernameSchemas.updateUsername,
     preHandler: meControllers.verifyAuthentication,
     handler: usernameControllers.updateUsername,
   });
