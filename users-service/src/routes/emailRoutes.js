9c3ba7e5e02d8f0d085034c8c148b2e68bdc9194
SalahHW <salah.bouheniche@gmail.com>
2025-07-14 23:47:16 +0200

    feat: move email update schema to a separate file for better organization

--- users-service/src/routes/emailRoutes.js
+++ users-service/src/routes/emailRoutes.js
@@ -1,20 +1,12 @@
 import * as emailControllers from "../controllers/emailControllers.js";
+import * as emailSchemas from "../schemas/emailSchema.js";
 import * as meControllers from "../controllers/meControllers.js";
 
 export default async function emailRoutes(fastify) {
   fastify.route({
     method: "PUT",
     url: "/users/email",
-    schema: {
-      body: {
-        type: "object",
-        required: ["email"],
-        properties: {
-          email: { type: "string", format: "email" },
-        },
-        additionalProperties: false,
-      },
-    },
+    schema: emailSchemas.updateEmail,
     preHandler: meControllers.verifyAuthentication,
     handler: emailControllers.updateEmail,
   });
