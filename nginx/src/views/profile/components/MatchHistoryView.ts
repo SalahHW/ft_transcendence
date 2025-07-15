62731e503340a85cdc53f2623dba23f82bbcaf5b
Matthieu De Langhe <111616290+Jaqensv@users.noreply.github.com>
2025-07-15 17:41:56 +0200

    Update MatchHistoryView.ts
    fix toLocaleString (en-GB) to fit with formattedDate

--- nginx/src/views/profile/components/MatchHistoryView.ts
+++ nginx/src/views/profile/components/MatchHistoryView.ts
@@ -93,7 +93,7 @@ export class MatchHistoryView {
 
         const date = new Date(endTimestamp * 1000);
         const day = date.getDate().toString().padStart(2, '0');
-        const month = date.toLocaleString('en-US', { month: 'short' });
+        const month = date.toLocaleString('en-GB', { month: 'short' });
         const year = date.getFullYear().toString().slice(-2);
         const formattedDate = `${day} ${month} ${year}`;
 
