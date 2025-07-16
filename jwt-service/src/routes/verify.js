ba95da70f0e86778554346e31654bc41624daf1c
SalahHW <salah.bouheniche@gmail.com>
2025-07-16 03:43:28 +0200

    feat: add checkBlacklistedTokens as preHandler for verify route

--- jwt-service/src/routes/verify.js
+++ jwt-service/src/routes/verify.js
@@ -1,9 +1,11 @@
 import { verifyToken } from "../controllers/verify.js";
+import { checkBlacklistedTokens } from "../controllers/redisControllers.js";
 
 export default async function verifyRoute(fastify) {
   fastify.route({
     method: "POST",
     url: "/verify",
+    preHandler: checkBlacklistedTokens,
     handler: verifyToken,
   });
 }
