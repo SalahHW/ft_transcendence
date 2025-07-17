92df6fc936e913ffcb9289e4401fd883e6b04546
joel688 <joachimkoenig68@icloud.com>
2025-07-17 16:53:26 +0200

    feat: enhance user creation logic to handle wallet authentication and remove unused imports

--- users-service/src/models/userModels.js
+++ users-service/src/models/userModels.js
@@ -1,7 +1,5 @@
 import { database } from "./database.js";
 import { translateSqliteError } from "./errors/translateSqliteError.js";
-import { createUsername } from "../controllers/usernameControllers.js";
-import { createEmail } from "../controllers/emailControllers.js";
 
 export const createUser = async ({
   username,
@@ -10,8 +8,6 @@ export const createUser = async ({
   wallet,
   authenticationMethod,
 }) => {
-  createUsername(username);
-  createEmail(email);
   const createUserQuery = `
     INSERT INTO users (username, authenticationMethod, wallet)
     VALUES (?, ?, ?);
