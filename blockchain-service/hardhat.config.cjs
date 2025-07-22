5b4248ded14285c7eeae88dc10e30c475f4ca05a
Jaqensv <matthieu.dl@hotmail.fr>
2025-07-22 11:24:46 +0200

    fix: pongToken ladder values recalculation + hardhat optimizer

--- blockchain-service/hardhat.config.cjs
+++ blockchain-service/hardhat.config.cjs
@@ -2,13 +2,20 @@ require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-    solidity: "0.8.20",
+    solidity: {
+        version: "0.8.20",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 100
+            }
+        }
+    },
     networks: {
         fuji: {
             url: "https://api.avax-test.network/ext/bc/C/rpc",
             chainId: 43113,
-            accounts: [
-                process.env.PRIVATE_KEY],
+            accounts: [process.env.PRIVATE_KEY],
         },
     },
     defaultNetwork: "fuji",
